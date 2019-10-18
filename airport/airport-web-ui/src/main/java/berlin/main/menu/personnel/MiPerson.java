f8f99cba36482e0921e1eef71c7d398d6244b4fa
myroslavarm <romaniuk@ucu.edu.ua>
2019-10-18 11:09:38 +0300

    initial project structure

---
+++ airport/airport-web-ui/src/main/java/berlin/main/menu/personnel/MiPerson.java
@@ -0,0 +1,16 @@
+package berlin.main.menu.personnel;
+
+import ua.com.fielden.platform.entity.annotation.EntityType;
+import ua.com.fielden.platform.ui.menu.MiWithConfigurationSupport;
+import berlin.personnel.Person;
+
+/**
+ * Main menu item representing an entity centre for {@link Person}.
+ *
+ * @author Generated
+ *
+ */
+@EntityType(Person.class)
+public class MiPerson extends MiWithConfigurationSupport<Person> {
+
+}
