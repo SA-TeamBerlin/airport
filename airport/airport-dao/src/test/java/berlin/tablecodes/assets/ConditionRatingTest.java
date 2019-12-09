fc242f424a1dbcd55effb74c45fea7cd9e2ae2a5
myroslavarm <romaniuk@ucu.edu.ua>
2019-12-10 01:41:03 +0200

    fixing ConditionRating test

--- airport/airport-dao/src/test/java/berlin/tablecodes/assets/ConditionRatingTest.java
+++ airport/airport-dao/src/test/java/berlin/tablecodes/assets/ConditionRatingTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 
 import berlin.tablecodes.conditions.ConditionRating;
 import berlin.tablecodes.conditions.IConditionRating;
+import berlin.tablecodes.services.IServiceStatus;
+import berlin.tablecodes.services.ServiceStatus;
 import berlin.test_config.AbstractDaoTestCase;
 import berlin.test_config.UniversalConstantsForTesting;
 import ua.com.fielden.platform.utils.IUniversalConstants;
@@ -16,12 +18,12 @@ public class ConditionRatingTest extends AbstractDaoTestCase {
 
     @Test
     public void test() {
-        final ConditionRating cr1 = co$(ConditionRating.class).findByKeyAndFetch(IConditionRating.FETCH_PROVIDER.fetchModel(), "CR1");
+        final ConditionRating cr1 = co$(ConditionRating.class).findByKeyAndFetch(IConditionRating.FETCH_PROVIDER.fetchModel(), 1);
         assertTrue(cr1.isValid().isSuccessful());
         
-        final ConditionRating cr2 = co(ConditionRating.class).findByKey("CR2");
+        final ConditionRating cr2 = co(ConditionRating.class).findByKey(3);
         assertNotNull(cr2);
-        assertEquals("CR2", cr2.getKey().toString());
+        assertEquals("3", cr2.getKey().toString());
     }
     
     @Override
@@ -44,9 +46,8 @@ public class ConditionRatingTest extends AbstractDaoTestCase {
         if (useSavedDataPopulationScript()) {
             return;
         }
-
-        // populating the data base for the test case
-        save(new_composite(ConditionRating.class, "CR1").setDesc("needs repair"));
-        save(new_composite(ConditionRating.class, "CR2").setDesc("operational"));
+        
+        save(new_composite(ConditionRating.class, 1).setDesc("to be repaired"));
+        save(new_composite(ConditionRating.class, 3).setDesc("operational"));
     }
 }
